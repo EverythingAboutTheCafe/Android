c9835bfe18bea1f517c6e018d7f82097261da477
pathpioneer <atn1su@gmail.com>
2022-06-11 21:59:15 +0900

    2022-06-11

--- app/src/main/java/com/camo/app/repository/HomeRepository.kt
+++ app/src/main/java/com/camo/app/repository/HomeRepository.kt
@@ -1,13 +1,19 @@
 package com.camo.app.repository
 
+import android.content.Context
+import com.camo.app.AssetLoader
 import com.camo.app.api.KakaoApi
+import com.camo.app.model.CafeData
 import com.camo.app.model.CafeInfo
+import com.camo.app.model.Timeline
 import com.camo.app.utils.Constants.Companion.CAFE_CODE
+import com.google.gson.Gson
 import retrofit2.Response
 import javax.inject.Inject
 
 class HomeRepository @Inject constructor(
-    private val kakaoApi: KakaoApi
+    private val kakaoApi: KakaoApi,
+    private val context: Context
 ) {
 
     suspend fun getNearbyCafes(radius: String, x: String, y: String, page: Int): Response<CafeInfo> {
